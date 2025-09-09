2ed038ac07b096ee96c9b83aea31fbbf4919c77a
ashu12658 <ashishghatol098@gmail.com>
2025-09-09 10:47:06 +0530

    Update orderoute.js

--- Cosmetics-store-Backend/routes/orderoute.js
+++ Cosmetics-store-Backend/routes/orderoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { protect, admin ,authUser } = require('../middleware/authMiddleware');
+const { protect, admin ,authUser } = require('../middleware/authmiddleware');
 const {
   getAllOrder,
   createOrder,
