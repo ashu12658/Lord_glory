e690df9e04fe836e649d11057fddd27552f5e8b5
ashu12658 <ashishghatol098@gmail.com>
2025-09-09 10:25:25 +0530

    Update productroute.js

--- Cosmetics-store-Backend/routes/productroute.js
+++ Cosmetics-store-Backend/routes/productroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { protect, admin } = require('../middleware/authmiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 const {
   getAllProducts,
   getProductById,
